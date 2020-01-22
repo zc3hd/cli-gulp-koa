8ce5227db3199783ebca2972ee46c529caaf6619
zc3hd <23397295+zc3hd@users.noreply.github.com>
2020-01-22 09:37:56 +0800

    upd:新增的其他文件类型

--- gulpfile.js
+++ gulpfile.js
@@ -138,7 +138,6 @@ Tool.prototype = {
   // 
   _compile: function(info) {
 
-    // 
     // 后缀
     switch (path.extname(info)) {
       case ".html":
@@ -150,9 +149,22 @@ Tool.prototype = {
       case ".js":
         this._js(info);
         break;
+      default:
+        this._other(info);
+        break;
+
     }
     this._pic(info);
   },
+  _other: function(info) {
+    gulp.src(info)
+      .pipe(plumber())
+      .pipe(gulp.dest(this._dist(info)))
+      .pipe(reload({
+        stream: true
+      }));
+  },
+
   _html: function(info) {
     gulp.src(info)
       .pipe(plumber())
